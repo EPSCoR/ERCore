d8d61bf6d49a293681eca4c749dd82770a2c869a
Andrew <shixish@gmail.com>
2013-05-15 10:03:18 -1000

    Hacked together a fancy tooltip widget. Using it for help text on the collaboration view. Hover over the question mark and it gives you some help text.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,12 +1,13 @@
 (function($){
-$(document).ready(function(){
-	$('form#user-profile-form').bind('state:visible', function(e) {
-		if (e.trigger) {
-			$(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut']();
-		}
+	$(document).ready(function(){
+		$('form#user-profile-form').bind('state:visible', function(e) {
+			if (e.trigger) {
+				$(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut']();
+			}
+		});
+		//$('#er-help h2').click(function(){
+		//	$(this).next().toggle();
+		//});
+		$('.help-tooltip').tooltip();
 	});
-	//$('#er-help h2').click(function(){
-	//	$(this).next().toggle();
-	//});
-});
 })(jQuery);
